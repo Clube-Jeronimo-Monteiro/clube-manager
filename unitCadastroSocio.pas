23bc0057be1642447febc58de67fb2f465ea1272
Guilherme Vianna <mataveli91@gmail.com>
2023-09-09 13:41:50 -0300

    feat: Cadastro de socio adicionado

---
+++ unitCadastroSocio.pas
@@ -0,0 +1,86 @@
+unit unitCadastroSocio;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.StdCtrls,
+  FireDAC.Stan.Intf, FireDAC.Stan.Option, FireDAC.Stan.Param,
+  FireDAC.Stan.Error, FireDAC.DatS, FireDAC.Phys.Intf, FireDAC.DApt.Intf,
+  FireDAC.Stan.Async, FireDAC.DApt, Data.DB, FireDAC.Comp.DataSet,
+  FireDAC.Comp.Client, FireDAC.UI.Intf, FireDAC.Stan.Def, FireDAC.Stan.Pool,
+  FireDAC.Phys, FireDAC.Phys.MySQL, FireDAC.Phys.MySQLDef, FireDAC.VCLUI.Wait;
+
+type
+  TForm2 = class(TForm)
+    Label1: TLabel;
+    Button1: TButton;
+    GroupBox1: TGroupBox;
+    Panel1: TPanel;
+    GroupBox2: TGroupBox;
+    Label2: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
+    Label5: TLabel;
+    EDName: TEdit;
+    EDCodSocio: TEdit;
+    EDBairro: TEdit;
+    EDRua: TEdit;
+    EDNumero: TEdit;
+    EDTelefone: TEdit;
+    Label6: TLabel;
+    Conexao: TFDConnection;
+    tbSocio: TFDTable;
+    tbSocioid: TFDAutoIncField;
+    tbSocionome: TStringField;
+    tbSociocodSocio: TIntegerField;
+    tbSociotelefone: TStringField;
+    tbSociobairro: TStringField;
+    tbSociorua: TStringField;
+    tbSocionumero: TStringField;
+    dsSocio: TDataSource;
+    FDQuery1: TFDQuery;
+    procedure Button1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form2: TForm2;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm2.Button1Click(Sender: TObject);
+begin
+
+
+   try
+    FDQuery1.SQL.Text := 'INSERT INTO socio (nome, codSocio, telefone, bairro, rua, numero) VALUES (:Nome, :codSocio, :Telefone, :Bairro, :Rua, :Numero)';
+
+  FDQuery1.ParamByName('Nome').AsString := EDName.Text;
+  FDQuery1.ParamByName('codSocio').AsInteger := StrToInt(EDCodSocio.Text);
+  FDQuery1.ParamByName('Telefone').AsString := EDTelefone.Text;
+  FDQuery1.ParamByName('Bairro').AsString := EDBairro.Text;
+  FDQuery1.ParamByName('Rua').AsString := EDRua.Text;
+  FDQuery1.ParamByName('Numero').AsString := EDNumero.Text;
+
+  FDQuery1.ExecSQL;
+
+  ShowMessage('Socio cadastrado com sucesso!');
+  Close;
+except
+  on E: Exception do
+  begin
+      ShowMessage('Ocorreu um erro, verifique os campos');
+  end;
+end;
+
+
+
+end;
+
+end.
---
+++ unitPrincipal.dfm
@@ -0,0 +1,44 @@
+object Form1: TForm1
+  Left = 0
+  Top = 0
+  Caption = 'Clube Campestre Manager'
+  ClientHeight = 808
+  ClientWidth = 1280
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -12
+  Font.Name = 'Segoe UI'
+  Font.Style = []
+  Menu = MainMenu1
+  TextHeight = 15
+  object PageControl1: TPageControl
+    Left = 8
+    Top = 8
+    Width = 1264
+    Height = 752
+    ActivePage = TabSheet1
+    TabOrder = 0
+    object TabSheet1: TTabSheet
+      Caption = 'Mensalidades'
+    end
+    object TabSheet2: TTabSheet
+      Caption = 'Exames de Pele'
+      ImageIndex = 1
+    end
+  end
+  object MainMenu1: TMainMenu
+    Left = 116
+    Top = 122
+    object Cadastrar1: TMenuItem
+      Caption = 'Cadastrar'
+      object Socio1: TMenuItem
+        Caption = 'Socio'
+        OnClick = Socio1Click
+      end
+      object Socio2: TMenuItem
+        Caption = 'Dependente'
+      end
+    end
+  end
+end
